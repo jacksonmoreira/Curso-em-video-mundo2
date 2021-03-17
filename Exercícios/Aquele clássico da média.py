34ec131ca743440e123b9693a929d8df19326e1b
jacksonmoreira <jacksonmoreiraneto@gmail.com>
2021-03-17 20:33:47 -0300

    Exercício 040

---
+++ Exercícios/Aquele clássico da média.py
@@ -0,0 +1,16 @@
+# Exercício 040
+print('-' * 15)
+print('''Média abaixo de 5.0: REPROVADO!
+Média entre 5.0 e 6.9: RECUPERAÇÃO!
+Média 7.0 ou superior: APROVADO!''')
+print('-' * 15)
+nota1 = float(input('Digite a sua primeira nota: '))
+nota2 = float(input('Digite a sua segunda nota: '))
+média = (nota1 + nota2) / 2
+print('A sua média é de {:.1f}!'.format(média))
+if média >= 7:
+    print('O aluno está APROVADO!')
+elif média > 5 and média < 6.9:
+    print('O aluno está em RECUPERAÇÃO!')
+elif média < 4.9:
+    print('O aluno está REPROVADO!')
