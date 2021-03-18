b3145da1ea415e430c63f4d5ffe09a87134fa62d
jacksonmoreira <jacksonmoreiraneto@gmail.com>
2021-03-18 09:18:30 -0300

    exercicio 042

--- Exercícios/Analisando triângulos v2.0.py
+++ Exercícios/Analisando triângulos v2.0.py
@@ -1,1 +1,31 @@
 # Exercíco 042
+import os, time
+os.system('clear')
+print('=-=' * 30)
+print('     ANALISANDO TRIÂNGULOS v2.0     ')
+print('=-=' * 30)
+print('''Digite o tamanho de três segmentos de reta
+e eu informarei se é possível formar um triângulo e
+também se ele é escaleno, isósceles e equilátero.''')
+print('-' * 15)
+seg1 = float(input('Digite o tamanho do segmento: '))
+seg2 = float(input('Digite o tamanho do segmento: '))
+seg3 = float(input('Digite o tamanho do segmento: '))
+print('-'  * 1)
+print('Analisando triângulo...')
+print('-' * 1)
+time.sleep(1)
+if seg1 < seg2 + seg3 and seg2 < seg1 + seg3 and seg3 < seg1 + seg2:
+    print('É possível formar um triângulo ', end='')
+    if seg1 == seg2 == seg3:
+        print('EQUILÁTERO!')
+    elif seg1 != seg2 != seg3 != seg1:
+        print('ESCALENO!')
+    else:
+        print('ISÓSCELES!')
+else:
+    print('Não é possível formar um triângulo!')
+print('-' * 1)
+time.sleep(1)
+input('Aperte enter para encerrar!')
+os.system('clear')
