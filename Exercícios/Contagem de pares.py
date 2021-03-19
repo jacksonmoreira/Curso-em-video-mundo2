cf6775cefda6f90001f8004fc7508da4994892a8
jacksonmoreira <jacksonmoreiraneto@gmail.com>
2021-03-19 14:21:17 -0300

    Contagem de pares.py

---
+++ Exercícios/Contagem de pares.py
@@ -0,0 +1,9 @@
+# Exercício 047
+import os
+os.system('clear')
+input('Aperte enter para iniciar!')
+for c in range(2, 51, 2):
+    print(c, end=' ')
+print('Acabou!')
+input('Aperte enter para encerrar!')
+os.system('clear')
